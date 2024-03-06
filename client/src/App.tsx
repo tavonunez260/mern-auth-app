6c17b8e76b3bc40d953952bce945d69d4fd6e112
Gustavo Nunez Ortiz <tavonunez260@gmail.com>
2024-03-06 18:15:49 -0500

    Header component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Header } from './components';
+import { About, Home, Profile, SignIn } from './pages';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route element={<Home />} path="/" />
+				<Route element={<About />} path="/about" />
+				<Route element={<Profile />} path="/profile" />
+				<Route element={<SignIn />} path="/sign-in" />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
---
+++ client/src/pages/index.ts
@@ -0,0 +1,4 @@
+export * from './Home.tsx';
+export * from './About.tsx';
+export * from './Profile.tsx';
+export * from './Signin.tsx';
