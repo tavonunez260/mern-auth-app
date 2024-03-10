e53845f105ad95fdf74d3a45d326622ee2e08844
Gustavo Nunez Ortiz <tavonunez260@gmail.com>
2024-03-10 00:31:53 -0500

    Complete image upload functionality.

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'store';
 import { SignUpForm } from 'types';
 import { rules } from 'utils';
 
+import { app } from '../firebase';
+
 export function Profile() {
 	const { currentUser } = useSelector(state => state.user);
 	const [loading, setLoading] = useState(false);
+	const [image, setImage] = useState<File | undefined>(undefined);
+	const [imagePercentage, setImagePercentage] = useState(0);
+	const [imageError, setImageError] = useState('');
+	const [downloadURL, setDownloadURL] = useState('');
+	const inputRef = useRef<HTMLInputElement>(null);
 	const {
 		formState: { errors },
 		handleSubmit,
@@ -22,16 +30,68 @@ export function Profile() {
 		reset();
 		setLoading(false);
 	};
+	const handleFileChange = useCallback(
+		async (image: File) => {
+			if (image.size > 2 * 1024 * 1024) {
+				setImageError('File size should not exceed 2MB');
+			} else {
+				if (imageError !== '') {
+					setImageError('');
+				}
+				const storage = getStorage(app);
+				const fileName = new Date().getTime() + image.name;
+				const storageRef = ref(storage, fileName);
+				const uploadTask = uploadBytesResumable(storageRef, image);
+
+				uploadTask.on(
+					'state_changed',
+					snapshot => {
+						setImagePercentage(Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100));
+					},
+					error => {
+						setImageError(`Error uploading file ${error.message}`);
+					},
+					() => {
+						getDownloadURL(uploadTask.snapshot.ref).then(downloadURL =>
+							setDownloadURL(downloadURL)
+						);
+					}
+				);
+			}
+		},
+		[imageError]
+	);
+
+	useEffect(() => {
+		if (image) {
+			handleFileChange(image);
+		}
+	}, [handleFileChange, image]);
 
 	return (
 		<main className="p-3 max-w-lg mx-auto">
 			<h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
 			<form className="flex flex-col gap-4" noValidate onSubmit={handleSubmit(onSubmit)}>
+				<input
+					ref={inputRef}
+					accept="image/*"
+					hidden
+					type="file"
+					onChange={event => setImage(event.target.files?.[0])}
+				/>
 				<img
 					alt={`${currentUser?.username} profile photo`}
 					className="w-24 h-24 mt-2 self-center cursor-pointer rounded-full object-cover"
-					src={currentUser?.profilePicture}
+					src={downloadURL || currentUser?.profilePicture}
+					onClick={() => inputRef?.current?.click()}
 				/>
+				{imageError && <p className="mt-2 text-sm text-red-600">{imageError}</p>}
+				{imagePercentage > 0 && imagePercentage <= 99 && (
+					<p className="mt-2 text-sm self-center text-slate-700">Uploading: {imagePercentage}%</p>
+				)}
+				{imagePercentage === 100 && (
+					<p className="mt-2 text-sm self-center text-green-700">Upload complete!</p>
+				)}
 				<div className="flex flex-col">
 					<input
 						{...register('username', {
